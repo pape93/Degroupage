b3afe25434e4c2cf0352d39439aec302d920e50e
Ernie Boland <pape93@gmail.com>
2023-04-06 22:53:16 -0400

    api path update on server side

--- src/server/app.js
+++ src/server/app.js
@@ -8,9 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Replace with your MongoDB connection string
 const uri = 'mongodb+srv://pape93:tTNvRtDrSxBRAEZC@cluster0.quynr8t.mongodb.net/degroup'; 
-//mongosh "mongodb+srv://cluster0.quynr8t.mongodb.net/Cluster0" --apiVersion 1 --username pape93 --password tTNvRtDrSxBRAEZC
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
@@ -19,36 +17,46 @@ connection.once('open', () => {
   console.log('MongoDB database connection established successfully');
 });
 
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-  
-    console.log('Received login request:', { username, password });
-  
-    try {
-      const user = await User.findOne({ username });
-  
-      console.log('Found user:', user);
-  
-      if (!user) {
-        return res.status(400).json({ message: 'Invalid username or password' });
-      }
-  
-      if (user.password !== password) {
-        return res.status(400).json({ message: 'Invalid username or password' });
-      }
-  
-      res.status(200).json({ message: 'Logged in successfully' });
-    } catch (error) {
-      console.error('Server error:', error);
-      res.status(500).json({ message: 'Server error' });
+const apiRouter = express.Router();
+
+apiRouter.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  console.log('Received login request:', { username, password });
+
+  try {
+    const user = await User.findOne({ username });
+
+    console.log('Found user:', user);
+
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid username or password' });
     }
-  });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: 'Invalid username or password' });
+    }
+
+    res.status(200).json({ message: 'Logged in successfully' });
+  } catch (error) {
+    console.error('Server error:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
   
+// Add any other API routes to the apiRouter as well
+// ...
+
+// Use the apiRouter for all /api routes
+app.use('/api', apiRouter);
+
 // Serve static files from the Angular app
 app.use(express.static(path.join(__dirname, '../../dist/degroupage')));
 
-// Handle any other routes (this should be the last route)
-app.get('*', (req, res) => {
+// Handle any other routes excluding /api routes (this should be the last route)
+app.get(/^(?!\/api).*$/, (req, res) => {
   res.sendFile(path.join(__dirname, '../../dist/degroupage/index.html'));
 });
 
